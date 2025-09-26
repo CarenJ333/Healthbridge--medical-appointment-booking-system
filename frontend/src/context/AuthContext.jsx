3e542bcd63d33cf2e69e2d2a7bd9dad3946900b3
Caren <carenjepchumba4@gmail.com>
2025-09-26 21:46:28 +0300

    misplaced link

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -23,7 +23,7 @@ export function AuthProvider({ children }) {
   }, [user]);
 
   const login = async (email, password) => {
-    const response = await fetch("http://127.0.0.1:5000/auth/login", {
+    const response = await fetch("http://healthbridge-medical-appointment.onrender.com/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
