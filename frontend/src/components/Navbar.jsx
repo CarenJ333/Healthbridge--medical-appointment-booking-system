a6639832ffda09f2b11e29b5184f6edf69f3a622
Caren <carenjepchumba4@gmail.com>
2025-09-28 20:18:45 +0300

    fix usercontext

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
-import { UserContext } from "../UserContext";
+import { UserContext } from "../UserContext.jsx";
 import "../App.css";
 
 export default function Navbar({ onLoginClick, onRegisterClick }) {
   const { user } = useContext(UserContext);
-  
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
